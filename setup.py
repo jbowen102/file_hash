56e6e13ad9efebb2179c0ee50ce0d09a3bf28d29
Jonathan Bowen <jjbowen19@gmail.com>
2017-08-27 21:12:41 -0400

    Add GitHub URL to setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ except ImportError:
 config = {
     'description': 'File Hash',
     'author': 'Jonathan Bowen',
-    'url': 'N/A'
-    'download_url': 'N/A',
+    'url': 'https://github.com/jbowen102/file_hash'
+    'download_url': '',
     'author_email': 'jjbowen19@gmail.com',
     'version': '0.1',
     'install_requires': ['nose'],
