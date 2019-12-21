ed0a1e6243fb88fe1daaa32647f54d760f6c08ee
Jonathan Bowen <ew15dro6k216@opayq.net>
2019-12-21 18:18:46 -0500

    Close file after open. Return hash hexdigest

--- bin/file_hash.py
+++ bin/file_hash.py
@@ -6,15 +6,15 @@ def file_hash(full_path, hash_type):
     Returns hash of file identified by full_path.
     Possible hash_types: 'md5', 'sha1', 'sha256' (passed as string)
     """
-    file_obj = open(full_path, 'rb')
+    with open(full_path, 'rb') as file_obj:
 
-    if hash_type == 'md5':
-        file_hash = hashlib.md5(file_obj.read())
-    elif hash_type == 'sha1':
-        file_hash = hashlib.sha1(file_obj.read())
-    elif hash_type == 'sha256':
-        file_hash = hashlib.sha256(file_obj.read())
-    else:
-        raise ValueError("Valid hash_types: 'md5', 'sha1', 'sha256'")
+        if hash_type == 'md5':
+            file_hash = hashlib.md5(file_obj.read())
+        elif hash_type == 'sha1':
+            file_hash = hashlib.sha1(file_obj.read())
+        elif hash_type == 'sha256':
+            file_hash = hashlib.sha256(file_obj.read())
+        else:
+            raise ValueError("Valid hash_types: 'md5', 'sha1', 'sha256'")
 
-    print(file_hash.hexdigest())
+        return file_hash.hexdigest()
